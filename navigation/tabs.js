3cf3c6035d9bb327b566978af4ffdf3e3aec575c
HOLEHUY <holehuy2003@gmail.com>
2021-02-23 20:46:45 +0700

    bottom tab navigation

---
+++ navigation/tabs.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
+
+import {
+  createBottomTabNavigator,
+  BottomTabBar,
+} from '@react-navigation/bottom-tabs';
+
+import {Home} from '../screens/';
+import {icons, COLORS} from '../constants/';
+
+const Tab = createBottomTabNavigator();
+const Tabs = () => {
+  return(
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} options={{
+        tabBarIcon:({focused})=>(
+          <Image source={icons.cutlery} resizeMode="contain" style={{width:25,height:25,tintColor:focused?COLORS.primary:COLORS.secondary}}/>
+        )
+      }}/>
+    </Tab.Navigator>
+  )
+};
+export default Tabs
