a9a7837831ebebe7e1147622a1f4917248347962
HOLEHUY <holehuy2003@gmail.com>
2021-02-24 09:44:58 +0700

    Update tabs.js

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -11,14 +11,85 @@ import {icons, COLORS} from '../constants/';
 
 const Tab = createBottomTabNavigator();
 const Tabs = () => {
-  return(
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} options={{
-        tabBarIcon:({focused})=>(
-          <Image source={icons.cutlery} resizeMode="contain" style={{width:25,height:25,tintColor:focused?COLORS.primary:COLORS.secondary}}/>
-        )
-      }}/>
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        showLabel: false,
+        style:{
+          backgroundColor:"transparent",
+          borderTopWidth:0,
+          elevation:0
+        }
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Image
+              source={icons.cutlery}
+              resizeMode="contain"
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? COLORS.primary : COLORS.secondary,
+              }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Home}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Image
+              source={icons.search}
+              resizeMode="contain"
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? COLORS.primary : COLORS.secondary,
+              }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Like"
+        component={Home}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Image
+              source={icons.like}
+              resizeMode="contain"
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? COLORS.primary : COLORS.secondary,
+              }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="User"
+        component={Home}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Image
+              source={icons.user}
+              resizeMode="contain"
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? COLORS.primary : COLORS.secondary,
+              }}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
-  )
+  );
 };
-export default Tabs
+export default Tabs;
